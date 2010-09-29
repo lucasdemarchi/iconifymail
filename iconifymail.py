d6de2cefbaea450e68e6b8f90e53edc897eb6320
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-09-29 20:37:34 -0300

    sanitize: use vars to group all the constants

--- iconifymail.py
+++ iconifymail.py
@@ -15,27 +15,34 @@ import Image, ImageFont, ImageDraw, ImageOps
 imgstr = sys.argv[1]
 addrstr = sys.argv[2]
 
+FONT = '/home/lucas/.fonts/EurostileLTStd-Demi.otf'
+FONTPXSIZE = 14
+COLOR_BACKGROUND= '#ffffff'
+COLOR_TEXT = '#3C465F'
+COLOR_BORDER = '#bebec7'
+IMG_OUTPUT = 'mail_' + addrstr + '.png'
+
 img_domain = Image.open(imgstr)
 w, h = img_domain.size
-print "Domain size: %dx%d" % (w, h)
 
-f = ImageFont.truetype('/home/lucas/.fonts/EurostileLTStd-Demi.otf', 14)
-
+f = ImageFont.truetype(FONT, FONTPXSIZE)
 wtxt, htxt = f.getsize(addrstr)
 
-print "height: txt: %d, img: %d pos: %d" % (htxt, h, (h - htxt) / 2)
+# + 2 for the border line
+wtxt += 2
 
+# ideally domain's height should be the biggest, but if it's not
+# at least we don't fail
 if htxt > h:
     h = htxt
-w = w + wtxt + 2
+w = w + wtxt
 
-img = Image.new('RGBA', (w, h), '#ffffff')
-
-img.paste(img_domain, (wtxt + 2, 0))
+img = Image.new('RGBA', (w, h), COLOR_BACKGROUND)
+img.paste(img_domain, (wtxt, 0))
 
 draw = ImageDraw.Draw(img)
-draw.text((2, h - htxt + 1 ), addrstr, "#3C465F", f)
-draw.rectangle([(0 ,0), (w - 1, h - 1)], outline="#bebec7")
+draw.text((2, h - htxt + 1 ), addrstr, COLOR_TEXT,f)
+draw.rectangle([(0 ,0), (w - 1, h - 1)], outline=COLOR_BORDER)
 
 
-img.save('mail_' + addrstr + '.png')
+img.save(IMG_OUTPUT)
