7a0a1d34990c39937662e0fd729a7142b12a9381
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-09-29 21:39:32 -0300

    Try hard to find a font

--- iconifymail.py
+++ iconifymail.py
@@ -3,29 +3,49 @@
 """
 Copyright (C) 2010 Lucas De Marchi <lucas.de.marchi@gmail.com>
 """
-import sys
-
-usage = "iconifymail domain_image name_address"
-if len(sys.argv) != 3:
-    print usage
-    sys.exit(1)
-
-import Image, ImageFont, ImageDraw, ImageOps
+import sys, os
 
 imgstr = sys.argv[1]
 addrstr = sys.argv[2]
 
-FONT = '/home/lucas/.fonts/EurostileLTStd-Demi.otf'
+FONT = 'EurostileLTStd-Demi.otf'
+FONT_DIRS = ['~/.fonts/', '/usr/share/fonts/']
 FONTPXSIZE = 14
 COLOR_BACKGROUND= '#ffffff'
 COLOR_TEXT = '#3C465F'
 COLOR_BORDER = '#bebec7'
 IMG_OUTPUT = 'mail_' + addrstr + '.png'
 
+
+usage = "iconifymail domain_image name_address"
+if len(sys.argv) != 3:
+    print usage
+    sys.exit(1)
+
+
+import Image, ImageFont, ImageDraw, ImageOps
+
+def load_font(FONT, FONT_DIRS, FONTPXSIZE):
+    font = None
+    for d in FONT_DIRS:
+        d = os.path.expanduser(d)
+        for dirpath, dirnames, filenames in os.walk(d):
+            for f in filenames:
+                if f == FONT:
+                    font = '/'.join([dirpath, f])
+                    break
+    if font:
+        return ImageFont.truetype(font, FONTPXSIZE)
+
+    return None
+
+f = load_font(FONT, FONT_DIRS, FONTPXSIZE)
+if not f:
+    raise Exception('Unable to find font %s' % (FONT))
+
 img_domain = Image.open(imgstr)
 w, h = img_domain.size
 
-f = ImageFont.truetype(FONT, FONTPXSIZE)
 wtxt, htxt = f.getsize(addrstr)
 
 # + 2 for the border line
